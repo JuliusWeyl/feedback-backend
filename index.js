7c98d62d422feeb1f4fe3f7edbc64b44dc7954cb
Julius Weyl <julius.weyl@haw-hamburg.de>
2019-11-20 20:51:53 +0100

    finish google authenitcation process, add dev script

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ const keys = require('./config/keys');
 
 const app = express();
 
+//todo add email registration and authentication
+
 passport.use(new GoogleStrategy(
     {
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
     }, 
-    accesToken => {
-        console.log(accesToken);
+    (accesToken, refreshTocken, profile, done) => {
+        console.log('access token',accesToken);
+        console.log('profile', profile);
     }
 ));
 
+//request authentication token
 app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }))
+
+//receive token for future communication
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
