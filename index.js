9657fa95c1ba4502634beb6409a187e69a12cf79
Julius Weyl <julius.weyl@haw-hamburg.de>
2019-11-25 07:04:43 +0100

    add mongoose to connect to MongoDB running on MongoDB Atlas

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+//mongoose.connect(keys.mongoURI,{ useNewUrlParser: true , useUnifiedTopology: true });
+
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => console.log('Error on start: ' + err.stack));
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
 //todo add email registration and authentication
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
